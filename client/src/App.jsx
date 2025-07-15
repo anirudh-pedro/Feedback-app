9e25a216e1eec165d11889ea0f325d4c773cb111
anirudh_pedro <anirudh200503@gmail.com>
2025-07-15 10:03:45 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='title bg-amber-500 '>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
